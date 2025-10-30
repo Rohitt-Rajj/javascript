1fee098a2f651eb934f7a670570299f06161710e
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-10-30 17:44:55 +0530

    variable

---
+++ variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345        
+// const  declare a constant variable, its value cannot be reassigned after its initial declaration.
+
+let accountEmail = "ranarohitsingh@gmail.com"
+// let have Block Scope
+
+var accountPassword = "12345"
+// Variables declared with the var always have Global Scope.
+// prefer not to use var because of issue in block scope & functional scope
+
+accountCity = "jaipur"
+// u can declare a variable like also but do not use.
+
+let accountState;
+
+accountEmail = "hshd@gmsil.com"
+accountPassword = "544662"
+accountCity = "bihar"
+
+console.log(accountEmail)
+console.table([accountState, accountPassword,accountEmail,accountCity])
