087039b8cc378413da363ceb249ebe871cb6e031
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-10-31 11:47:58 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,28 @@
+let score = "33"
+console.log(typeof score)
+
+// convert string to number
+let valueInNumber =  Number(score)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let conversion = Boolean(isLoggedIn)
+console.log(typeof conversion)
+console.log(conversion)
+
+// "" => false
+// "Rohit" => true
+
+
+let someNumber = 33
+let strigNumber =  String(someNumber)
+
+console.log(strigNumber);
+console.log(typeof strigNumber);
+
+
+
