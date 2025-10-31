f1be8384df1d08c026faae0cf91d649ad8532ad0
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-10-31 14:17:03 +0530

    comparision.js

---
+++ comparision.js
@@ -0,0 +1,20 @@
+console.log(2 > 1); //true
+console.log("1" > 0); //do not gives error but it is not the good method, does not gives u predictable result
+
+console.log(null > 0); //false
+console.log(null < 0); //false
+console.log(null >= 0); //true
+
+
+// undefined always gives u false 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+/// ===  check data type also
+
+console.log("2" === 2); //false 
+
+
+
