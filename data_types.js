1301e3bd17927e5d6f9faa6ac7a42398d3cba7f6
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-10-31 11:29:04 +0530

    data_types.js

---
+++ data_types.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all the js code as newer version
+
+// alert(3 + 3)  we are using node js not browser
+
+let name = "Rohit"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// biginit
+// string is used in single or double quotes also
+// boolean => true or false
+// null => standalone value , u can asign null
+// undefined => when u can declare only variable , do not assign any value 
+// symbol = unique
+
+// object
+ 
+
+console.log(typeof null)     // object
+console.log(typeof undefined)   
