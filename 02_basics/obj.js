86a5f4d7302800684dfb62fe0fd4e5a28627f848
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-11-10 15:30:27 +0530

    obj

--- 02_basics/obj.js
+++ 02_basics/obj.js
@@ -30,5 +30,35 @@ const obj1 = {1: "a", 2: "b"}   //1 is key and 2 is value
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-const obj3 = {obj1,obj2}
+//const obj3 = {obj1,obj2}  // storing obj1 & obj2 in obj 3 
 console.log(obj3);
+
+//const obj3 = Object.assign({}, obj1, obj2, obj4)   // here {} means empty target
+// Object.assign(target, source1, source2, ...)
+const obj3 = {...obj1, ...obj2} //best approach
+
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
