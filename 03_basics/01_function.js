d859aca4e1e6cb5ea0ffddd2dbc7abd3ca4b61ef
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-11-10 16:41:13 +0530

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -47,3 +47,14 @@ console.log(`User name is ${anyobject.username} and their age is ${anyobject.age
 }
 
 userDetail(userDetails);
+
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
