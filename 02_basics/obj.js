4a9f9c0a7171a371586816f65c7c3a4d2d245ec7
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-11-10 15:43:23 +0530

    obj

--- 02_basics/obj.js
+++ 02_basics/obj.js
@@ -62,3 +62,31 @@ users[1].email
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+ 
+
+// json file format may be object or array
+[
+    {},
+    {},
+    {}
+]
