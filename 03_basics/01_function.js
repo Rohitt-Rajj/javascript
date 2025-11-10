6095f7cf12a3cafd95548a3bdf07facbe7ac6ee0
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-11-10 16:39:24 +0530

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,49 @@
+function saymyname(){
+    console.log("R");
+    console.log("O");
+    console.log("H");
+    console.log("I");
+    console.log("T");
+}
+saymyname();
+
+
+
+function addTwoNumber(num1 , num2){
+        //console.log(num1 + num2);
+        return num1 + num2
+        
+}
+//addTwoNumber(3,5);
+const result = addTwoNumber(3,5)
+console.log("Result: ", result);  
+
+
+function login(userinput){
+    if(userinput == undefined){
+        return `Please enter username`
+    }
+    return ` ${userinput} just logged in`
+}
+console.log(login('rohit'));
+
+
+
+function calculatecartPrice(...nums1){  //  ... is known as rest or spread operator
+    return nums1
+
+}
+console.log(calculatecartPrice(100,200,300));
+
+
+
+const userDetails = {
+    username: "Rohit",
+    age: 21
+}
+
+function userDetail(anyobject){
+console.log(`User name is ${anyobject.username} and their age is ${anyobject.age}`);
+}
+
+userDetail(userDetails);
