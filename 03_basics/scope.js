865f0a7b799f2bcb88309f14e61522912be0a1eb
Rohitt-Rajj <rohitsinghrajput62020@gmail.com>
2025-11-13 21:47:17 +0530

    scope

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -2,3 +2,31 @@
 // avoid var because it is 
 
 
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// both are the way to declRE  a function
+
+console.log(addone(5))      // here you can print function before assigning
+
+function addone(num){
+    return num + 1
+}
+
+
+
+
+//  console.log(addTwo(5))   you can not access this before assinging the function like this
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))      // u have to use like this 
